8f2677de1c7bae9e9f8f4ce302c7b189de561768
Sami-zzz <2787621724@qq.com>
2024-04-05 21:21:05 +0800

    直播间礼物列表完成

--- src/views/pull/index.vue
+++ src/views/pull/index.vue
@@ -16,7 +16,100 @@
         autoplay
       ></video>
     </div>
-    <div class="gift__container"></div>
+    <div class="gift__container">
+      <div class="gift__header">为喜爱的主播送出礼物</div>
+      <ul>
+        <li
+          class="gift-item"
+          @click="showModal(1, '小花花', 1)"
+        >
+          <img
+            src="../../assets/img/gift/1.png"
+            alt=""
+          />
+          <div class="gift-info">
+            <div class="gift-title">小花花</div>
+            <div class="gift-price">1金币</div>
+          </div>
+        </li>
+        <li
+          class="gift-item"
+          @click="showModal(2, '牛哇牛哇', 10)"
+        >
+          <img
+            src="../../assets/img/gift/2.png"
+            alt=""
+          />
+          <div class="gift-info">
+            <div class="gift-title">牛哇牛哇</div>
+            <div class="gift-price">10金币</div>
+          </div>
+        </li>
+        <li
+          class="gift-item"
+          @click="showModal(3, '666', 66)"
+        >
+          <img
+            src="../../assets/img/gift/3.png"
+            alt=""
+          />
+          <div class="gift-info">
+            <div class="gift-title">666</div>
+            <div class="gift-price">66金币</div>
+          </div>
+        </li>
+        <li
+          class="gift-item"
+          @click="showModal(4, '高能预警', 100)"
+        >
+          <img
+            src="../../assets/img/gift/4.png"
+            alt=""
+          />
+          <div class="gift-info">
+            <div class="gift-title">高能预警</div>
+            <div class="gift-price">100金币</div>
+          </div>
+        </li>
+        <li
+          class="gift-item"
+          @click="showModal(5, '超级跑车', 300)"
+        >
+          <img
+            src="../../assets/img/gift/5.png"
+            alt=""
+          />
+          <div class="gift-info">
+            <div class="gift-title">超级跑车</div>
+            <div class="gift-price">300金币</div>
+          </div>
+        </li>
+        <li
+          class="gift-item"
+          @click="showModal(6, '次元之城', 1000)"
+        >
+          <img
+            src="../../assets/img/gift/6.png"
+            alt=""
+          />
+          <div class="gift-info">
+            <div class="gift-title">次元之城</div>
+            <div class="gift-price">1000金币</div>
+          </div>
+        </li>
+      </ul>
+      <a-modal
+        v-model:open="giftOpen"
+        title="确定送礼"
+        @ok="handleGift"
+        class="gift-modal"
+      >
+        <p class="gift-modal__content">
+          您确定要送出<span>{{ currentGiftName }}</span
+          >吗, 价值<span>{{ currentPrice }}金币</span>
+        </p>
+      </a-modal>
+    </div>
   </div>
 </template>
 
@@ -33,6 +126,11 @@ const pullUrl = ref();
 const title = ref();
 const open_time = ref();
 const username = ref();
+const giftOpen = ref(false);
+const currentGiftId = ref();
+const currentGiftName = ref();
+const currentPrice = ref();
+// 拉流
 const pullVideo = async () => {
   let user_id = route.query.user_id as string;
   if (!user_id) {
@@ -48,6 +146,7 @@ const pullVideo = async () => {
     window.$message.error('错误');
   }
 };
+// 主播信息
 const getUserInfo = async () => {
   let user_id = route.query.user_id as string;
   if (!user_id) {
@@ -70,6 +169,18 @@ const format = (timestamp) => {
   return new Date(parseInt(timestamp) * 1000)
     .toLocaleString()
     .replace(/:\d{1,2}$/, ' ');
+};
+// 点击送礼
+
+const showModal = (giftId, giftName, price) => {
+  currentGiftName.value = giftName;
+  currentGiftId.value = giftId;
+  currentPrice.value = price;
+  giftOpen.value = true;
+};
+
+const handleGift = (e: MouseEvent) => {
+  giftOpen.value = false;
 };
 
 onMounted(() => {
@@ -102,7 +213,7 @@ onMounted(() => {
 
     .main__header {
       height: 10%;
-      background-color: #385178;
+      background-color: #121750;
       z-index: 1000;
       border-radius: 10px 10px 0px 0px;
       display: flex;
@@ -130,7 +241,66 @@ onMounted(() => {
     height: 650px;
     width: 250px;
     border-radius: 10px;
-    background-color: pink;
+    background-color: #121750;
+    overflow: hidden;
+
+    .gift__header {
+      height: 50px;
+      font-size: 20px;
+      font-weight: 700;
+      color: #fff;
+      background-color: #121750;
+      line-height: 50px;
+      text-align: center;
+    }
+
+    .gift-item {
+      margin: 0 auto;
+      margin-top: 10px;
+      height: 88px;
+      width: 90%;
+      border-radius: 10px;
+      overflow: hidden;
+      display: flex;
+      cursor: pointer;
+
+      align-items: center;
+      border: 2px solid #121750;
+      img {
+        width: 70px;
+        height: 70px;
+        margin-left: 20px;
+        border-radius: 5px;
+      }
+
+      .gift-info {
+        margin-left: 25px;
+        .gift-title {
+          font-size: 18px;
+          color: #fff;
+          padding-bottom: 5px;
+        }
+        .gift-price {
+          font-size: 16px;
+          color: #999;
+        }
+      }
+
+      &:hover {
+        border: #d44e7d 2px solid;
+        .gift-title {
+          color: #d44e7d;
+        }
+      }
+    }
+  }
+}
+.gift-modal {
+  .gift-modal__content {
+    font-size: 16px;
+    span {
+      color: #d44e7d;
+    }
   }
 }
 </style>
